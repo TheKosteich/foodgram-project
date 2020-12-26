37136343623793f075399619247d79b0b03ce196
Konstantin Tornovskiy <coolship@yandex.ru>
2020-12-26 10:44:22 +0300

    Temp commit

--- recipes/models.py
+++ recipes/models.py
@@ -44,3 +44,16 @@ class RecipeIngredients(models.Model):
 
     def __str__(self):
         return f'{self.recipe} - {self.ingredient}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorites'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='choosers'
+    )
