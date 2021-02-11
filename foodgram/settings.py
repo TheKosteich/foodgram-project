1d515a0b32b6b15d4f7d0499025c71d1cbab55fd
Konstantin Tornovskiy <coolship@yandex.ru>
2021-02-11 13:18:56 +0300

    Configure settings.py

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -12,9 +12,9 @@ try:
     DATABASES = {
         'default': {
             'ENGINE': os.environ['DB_ENGINE'],
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD'],
+            'NAME': os.environ['POSTGRES_DB'],
+            'USER': os.environ['POSTGRES_USER'],
+            'PASSWORD': os.environ['POSTGRES_PASSWORD'],
             'HOST': os.environ['DB_HOST'],
             'PORT': os.environ['DB_PORT'],
         }
