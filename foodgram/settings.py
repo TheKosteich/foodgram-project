aafb5bea7b77043b15b35fc20b38e33f1436ee2c
Konstantin Tornovskiy <coolship@yandex.ru>
2021-02-11 14:01:15 +0300

    Test commit

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -36,9 +36,6 @@ except KeyError:
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 
-    # Database
-    # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
